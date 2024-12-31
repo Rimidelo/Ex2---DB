0c3af620b373f40e517c867520ecc2692b44158a
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2024-12-31 19:50:31 +0200

    started the student enrollment to course

---
+++ server/controllers/student_controller.js
@@ -0,0 +1,48 @@
+import Student from '../models/student.js';
+import Course from '../models/course.js';
+
+export const enrollInCourse = async (req, res) => {
+    const studentId = req.user.id;
+    const { course_id } = req.params;
+
+    try {
+        const student = await Student.findById(studentId);
+        if (!student) {
+            return res.status(404).json({ error: 'Student not found' });
+        }
+
+        const course = await Course.findById(course_id);
+        if (!course) {
+            return res.status(404).json({ error: 'Course not found' });
+        }
+
+        if (course.numOfStudents >= course.maxStudents) {
+            return res.status(400).json({ error: 'Course is full' });
+        }
+
+        if (student.courses.includes(course_id)) {
+            return res.status(400).json({ error: 'Student is already enrolled in this course' });
+        }
+
+
+        const totalCredits = student.numOfPoints + course.creditPoints;
+        if (totalCredits > 20) {
+            return res.status(400).json({ error: 'Enrolling in this course exceeds the credit limit of 20 points' });
+        }
+
+        student.courses.push(course_id);
+        student.numOfPoints = totalCredits;
+
+        course.studentsList.push(student._id);
+        course.numOfStudents += 1;
+
+        await student.save();
+        await course.save();
+
+
+        res.status(200).json({ message: 'Student successfully enrolled in the course', student });
+    } catch (err) {
+        console.error('Error enrolling student:', err);
+        res.status(500).json({ error: 'Server error during enrollment' });
+    }
+};
--- server/routers/courses_routes.js
+++ server/routers/courses_routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authenticateToken, authorizeRole } from '../middleware/tokenValidation.js';
 import { createCourse,getCourses, updateCourse, deleteCourse} from '../controllers/courses_controller.js';
+import { enrollInCourse } from '../controllers/student_controller.js';
 
 const router = express.Router();
 
@@ -8,5 +9,7 @@ router.post('/create', authenticateToken, authorizeRole(['staff']), createCourse
 router.get('/', authenticateToken, authorizeRole(['staff']), getCourses);
 router.put('/:id', authenticateToken, authorizeRole(['staff']), updateCourse);
 router.delete('/:id', authenticateToken, authorizeRole(['staff']), deleteCourse);
+
+router.post('/courses/:course_id', authenticateToken, authorizeRole(['student']), enrollInCourse);
 
 export default router;
