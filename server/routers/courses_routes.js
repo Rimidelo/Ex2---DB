76ac5750065ce5e5a1b93a90b14504517bd28b24
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2025-01-02 19:12:46 +0200

    arranged the routes for redability

--- server/routers/courses_routes.js
+++ server/routers/courses_routes.js
@@ -1,20 +1,24 @@
 import express from 'express';
 import { authenticateToken, authorizeRole } from '../middleware/tokenValidation.js';
-import { createCourse, getCourses, updateCourse, deleteCourse, getSingleCourse } from '../controllers/courses_controller.js';
+import {
+    createCourse,
+    getCourses,
+    updateCourse,
+    deleteCourse,
+    getSingleCourse,
+} from '../controllers/courses_controller.js';
 import { enrollInCourse, dropFromCourse } from '../controllers/student_controller.js';
 
 const router = express.Router();
 
-router.post('/create', authenticateToken, authorizeRole(['staff']), createCourse);
 router.get('/', authenticateToken, getCourses);
+router.get('/:course_id', authenticateToken, getSingleCourse);
+
+router.post('/create', authenticateToken, authorizeRole(['staff']), createCourse);
 router.put('/:id', authenticateToken, authorizeRole(['staff']), updateCourse);
 router.delete('/:id', authenticateToken, authorizeRole(['staff']), deleteCourse);
 
-router.get('/:course_id', authenticateToken, getSingleCourse);
-
 router.post('/:course_id', authenticateToken, authorizeRole(['student']), enrollInCourse);
 router.delete('/:course_id', authenticateToken, authorizeRole(['student']), dropFromCourse);
-
-
 
 export default router;
